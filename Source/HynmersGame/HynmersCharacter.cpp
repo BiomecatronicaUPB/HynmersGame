1752e6457cbc96d318364bcf4e3348f3a5d5cf63
Juan Franco <juancaf93@hotmail.com>
2018-06-03 23:58:52 -0500

    Added Hynmers classes
    - Character
    - Camera component
    - Movement Component

---
+++ Source/HynmersGame/HynmersCharacter.cpp
@@ -0,0 +1,208 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "HynmersCharacter.h"
+#include "HynmersCameraComponent.h"
+#include "HynmersMovementComponent.h"
+
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "GameFramework/InputSettings.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Kismet/GameplayStatics.h"
+#include "MotionControllerComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/PoseableMeshComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Engine/World.h"
+
+#include "Paths.h"
+#include <ctime>
+
+#define NUM_ACCELS 3
+#define NUM_AXES 2
+
+using namespace std;
+
+DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
+
+
+//////////////////////////////////////////////////////////////////////////
+// AHynmersCharacter
+
+AHynmersCharacter::AHynmersCharacter(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer.SetDefaultSubobjectClass<UHynmersMovementComponent>(ACharacter::CharacterMovementComponentName))
+{
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(55.f, 96.0f);
+
+	// set our turn rates for input
+	BaseTurnRate = 45.f;
+	BaseLookUpRate = 45.f;
+
+	// Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+	Mesh = GetMesh();
+	Mesh->SetOnlyOwnerSee(true);
+	Mesh->SetupAttachment(GetCapsuleComponent());
+	Mesh->bCastDynamicShadow = false;
+	Mesh->CastShadow = false;
+	//Mesh->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	//Mesh->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+
+	BoneMesh = CreateOptionalDefaultSubobject<UPoseableMeshComponent>(TEXT("BoneMesh"));
+	if (BoneMesh)
+	{
+		BoneMesh->AlwaysLoadOnClient = true;
+		BoneMesh->AlwaysLoadOnServer = true;
+		BoneMesh->bOwnerNoSee = false;
+		BoneMesh->MeshComponentUpdateFlag = EMeshComponentUpdateFlag::AlwaysTickPose;
+		BoneMesh->bCastDynamicShadow = true;
+		BoneMesh->bAffectDynamicIndirectLighting = true;
+		BoneMesh->PrimaryComponentTick.TickGroup = TG_PrePhysics;
+		BoneMesh->SetupAttachment(GetCapsuleComponent());
+		static FName MeshCollisionProfileName(TEXT("CharacterMesh"));
+		BoneMesh->SetCollisionProfileName(MeshCollisionProfileName);
+		BoneMesh->bGenerateOverlapEvents = false;
+		BoneMesh->SetCanEverAffectNavigation(false);
+	}
+
+	// Create a CameraComponent	
+	RootCameraComponent = CreateDefaultSubobject<USceneComponent>(TEXT("FirstPersonCameraRoot"));
+	RootCameraComponent->SetupAttachment(BoneMesh, FName("CameraPoint"));
+	RootCameraComponent->RelativeRotation = FRotator(-6.5f, 56.f, -90.f);
+
+	FirstPersonCameraComponent = CreateDefaultSubobject<UHynmersCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(RootCameraComponent);
+	FirstPersonCameraComponent->RelativeLocation = FVector(16.65f, 0.8f, 15.53f); // Position the camera
+	FirstPersonCameraComponent->bUsePawnControlRotation = false;
+	FirstPersonCameraComponent->bLockToHmd = false;
+	FirstPersonCameraComponent->bLockToOculus = true;
+	
+	
+	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
+	// are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
+
+	// Create VR Controllers.
+	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));/*
+	R_MotionController->Hand = EControllerHand::Right;*/
+	R_MotionController->SetupAttachment(RootComponent);
+	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
+	L_MotionController->SetupAttachment(RootComponent);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+void AHynmersCharacter::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+}
+
+void AHynmersCharacter::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	FName BonesNames[] = { FName("thigh_l"), FName("calf_l"), FName("thigh_r"), FName("calf_r"), FName("foot_l"), FName("foot_r") };
+
+	for (int i = 0; i < 4; i++) {
+		FRotator BoneRotation = (i >= 2) ? FRotator(-90 - Rate[i], 90, 90) : FRotator(90 - Rate[i], 90, 90);
+		BoneMesh->SetBoneRotationByName(BonesNames[i], BoneRotation, EBoneSpaces::ComponentSpace);
+	}
+
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+// Input
+
+void AHynmersCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	// set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	PlayerInputComponent->BindAction("Exit", IE_Pressed, this, &AHynmersCharacter::OnExit);
+
+	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AHynmersCharacter::OnResetVR);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AHynmersCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AHynmersCharacter::MoveRight);
+
+	// We have 2 versions of the rotation bindings to handle different kinds of devices differently
+	// "turn" handles devices that provide an absolute delta, such as a mouse.
+	// "turnrate" is for devices that we choose to treat as a rate of change, such as an analog joystick
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("TurnRate", this, &AHynmersCharacter::TurnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookUpRate", this, &AHynmersCharacter::LookUpAtRate);
+
+
+	PlayerInputComponent->BindAxis("PosLeftThight", this, &AHynmersCharacter::PosLeftThight);
+	PlayerInputComponent->BindAxis("PosLeftKnee", this, &AHynmersCharacter::PosLeftKnee);
+	PlayerInputComponent->BindAxis("PosRightThight", this, &AHynmersCharacter::PosRightThight);
+	PlayerInputComponent->BindAxis("PosRightKnee", this, &AHynmersCharacter::PosRightKnee);
+}
+
+
+void AHynmersCharacter::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+void AHynmersCharacter::MoveForward(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetRootComponent()->GetForwardVector(), Value);
+	}
+}
+
+void AHynmersCharacter::MoveRight(float Value)
+{
+	if (Value != 0.0f)
+	{
+		// add movement in that direction
+		AddMovementInput(GetRootComponent()->GetRightVector(), Value);
+	}
+}
+
+void AHynmersCharacter::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AHynmersCharacter::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AHynmersCharacter::OnExit(void)
+{
+	UKismetSystemLibrary::QuitGame(GetWorld(), UGameplayStatics::GetPlayerController(GetWorld(), 0), EQuitPreference::Quit);
+}
+
+void AHynmersCharacter::PosLeftThight(float rate)
+{
+	Rate[0] = rate * 180.f / PI;
+}
+
+void AHynmersCharacter::PosLeftKnee(float rate)
+{
+	Rate[1] = rate * 180.f / PI;
+}
+
+void AHynmersCharacter::PosRightThight(float rate)
+{
+	Rate[2] = rate * 180.f / PI;
+}
+
+void AHynmersCharacter::PosRightKnee(float rate)
+{
+	Rate[3] = rate * 180.f / PI;
+}
---
+++ Source/HynmersGame/HynmersMovementComponent.h
@@ -0,0 +1,108 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "HynmersMovementComponent.generated.h"
+
+/*
+ * 
+ */
+UCLASS()
+class HYNMERSGAME_API UHynmersMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+
+/*Revision:
+	TickComponent
+		PerformMovement
+			PhysSwimming
+*/
+public:
+
+	UHynmersMovementComponent();
+	
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	virtual void PerformMovement(float DeltaSeconds) override;
+
+	//Movements
+	virtual void PhysWalking(float deltaTime, int32 Iterations) override;
+
+	// Function in charged of move the updated component
+	virtual void MoveAlongFloor(const FVector& InVelocity, float DeltaSeconds, FStepDownResult* OutStepDownResult = NULL) override;
+
+	virtual FVector GetLedgeMove(const FVector& OldLocation, const FVector& Delta, const FVector& GravDir) const;
+
+	virtual bool ComputePerchResult(const float TestRadius, const FHitResult& InHit, const float InMaxFloorDist, FFindFloorResult& OutPerchFloorResult) const;
+
+	// function that clamp acceleration in Z
+	virtual FVector ConstrainInputAcceleration(const FVector& InputAcceleration) const override;
+
+	// Function that clamps velocity in Z
+	virtual void MaintainHorizontalGroundVelocity() override;
+
+	virtual FVector ComputeGroundMovementDelta(const FVector& Delta, const FHitResult& RampHit, const bool bHitFromLineTrace) const;
+
+	bool SafeMoveUpdatedComponent(const FVector& Delta, const FQuat& NewRotation, bool bSweep, FHitResult& OutHit, ETeleportType Teleport = ETeleportType::None);
+
+	bool MoveUpdatedComponent(const FVector& Delta, const FQuat& NewRotation, bool bSweep, FHitResult* OutHit = NULL, ETeleportType Teleport = ETeleportType::None);
+
+	virtual bool StepUp(const FVector& GravDir, const FVector& Delta, const FHitResult &Hit, struct UCharacterMovementComponent::FStepDownResult* OutStepDownResult = NULL) override;
+
+	virtual bool IsWithinEdgeTolerance(const FVector& CapsuleLocation, const FVector& TestImpactPoint, const float CapsuleRadius) const;
+
+	virtual void AdjustFloorHeight();
+
+	virtual void HandleImpact(const FHitResult& Hit, float TimeSlice = 0.f, const FVector& MoveDelta = FVector::ZeroVector) override;
+
+	virtual void ApplyImpactPhysicsForces(const FHitResult& Impact, const FVector& ImpactAcceleration, const FVector& ImpactVelocity);
+
+	virtual float SlideAlongSurface(const FVector& Delta, float Time, const FVector& Normal, FHitResult& Hit, bool bHandleImpact) override;
+
+	// Floor Finding functions
+	virtual void FindFloor(const FVector& CapsuleLocation, FFindFloorResult& OutFloorResult, bool bZeroDelta, const FHitResult* DownwardSweepResult = NULL) const override;
+
+	virtual void ComputeFloorDist(const FVector& CapsuleLocation, float LineDistance, float SweepDistance, FFindFloorResult& OutFloorResult, float SweepRadius, const FHitResult* DownwardSweepResult = NULL) const override;
+
+	virtual bool FloorSweepTest(struct FHitResult& OutHit, const FVector& Start, const FVector& End, ECollisionChannel TraceChannel, const struct FCollisionShape& CollisionShape,
+		const struct FCollisionQueryParams& Params, const struct FCollisionResponseParams& ResponseParam) const override;
+
+	virtual bool IsWalkable(const FHitResult& Hit) const override;
+
+	virtual void PhysFalling(float deltaTime, int32 Iterations) override;
+
+	virtual FVector GetFallingLateralAcceleration(float DeltaTime) override;
+
+	virtual bool DoJump(bool bReplayingMoves) override;	
+	
+	virtual float BoostAirControl(float DeltaTime, float TickAirControl, const FVector& FallAcceleration) override;
+
+	virtual void ProcessLanded(const FHitResult& Hit, float remainingTime, int32 Iterations) override;
+
+	virtual bool IsValidLandingSpot(const FVector& CapsuleLocation, const FHitResult& Hit) const override;
+
+	virtual void SetPostLandedPhysics(const FHitResult& Hit) override;
+
+	virtual FVector LimitAirControl(float DeltaTime, const FVector& FallAcceleration, const FHitResult& HitResult, bool bCheckForValidLandingSpot) override;
+
+	virtual FVector HandleSlopeBoosting(const FVector& SlideResult, const FVector& Delta, const float Time, const FVector& Normal, const FHitResult& Hit) const;
+
+	virtual void TwoWallAdjust(FVector& Delta, const FHitResult& Hit, const FVector& OldHitNormal) const override;
+
+	virtual void PhysSwimming(float deltaTime, int32 Iterations) override;
+
+	void StartSwimming(FVector OldLocation, FVector OldVelocity, float timeTick, float remainingTime, int32 Iterations);
+
+	virtual float ImmersionDepth() const override;
+
+	// Angular velocity in degrees per second
+	UPROPERTY(Category = "Character Movement: Walking", EditAnywhere, BlueprintReadWrite, meta = (ClampMin = "0", UIMin = "0"))
+		float AngularVelocity = 90.f;
+
+private:
+	FVector UpVector;
+	FVector RightVector;
+	FVector ForwardVector;
+};
