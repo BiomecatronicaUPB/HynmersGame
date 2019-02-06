e1d60890894c8f6328493330bf481c1456004d77
Juan Franco <juancaf93@hotmail.com>
2019-02-05 23:23:35 -0500

    Enhanced bridge and machines maps

--- Source/HynmersGame/HynmersGameGameMode.cpp
+++ Source/HynmersGame/HynmersGameGameMode.cpp
@@ -15,8 +15,8 @@ AHynmersGameGameMode::AHynmersGameGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Test/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
-	DefaultPawnClass = PlayerPawnClassFinder.Class;
+	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Test/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	//DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
 	HUDClass = AHynmersGameHUD::StaticClass();
